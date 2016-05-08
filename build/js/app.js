85e782b88859fb15bfb3c2381b6639b64c438acb
Joseph Nwachukwu <josephnwachukwu@gmail.com>
2016-05-08 18:49:41 -0400

    boilerplate v1
    this is a boilerplate that follows the latest best practices as well as
    my preferences for front end development. as of 5/16.

---
+++ build/js/app.js
@@ -0,0 +1,17 @@
+//Angular Boilerplate App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'boilerplate' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'boilerplate.controllers' is found in controllers.js
+
+angular.module('boilerplate', [])
+
+
+.run(function(){
+
+})
+
+.config(function(){
+
+});
---
+++ build/js/controllers/footer.ctrl.js
@@ -0,0 +1,3 @@
+angular.controller(['footer', function(){
+	
+}]);
